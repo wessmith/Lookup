5035e92e2a8d56142f5bebb8cd135177ff19d04e
Wes Smith <virtualaudio@gmail.com>
2012-09-25 12:39:05 -0400

    Added Nimbus pod

---
+++ Controllers/LUPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  LUPhotoViewController.h
+//  Lookup
+//
+//  Created by Wesley Smith on 9/25/12.
+//  Copyright (c) 2012 Wesley Smith. All rights reserved.
+//
+
+#import "NIToolbarPhotoViewController.h"
+
+@interface LUPhotoViewController : NIToolbarPhotoViewController
+
+@end
---
+++ Controllers/LUPhotoViewController.m
@@ -0,0 +1,38 @@
+//
+//  LUPhotoViewController.m
+//  Lookup
+//
+//  Created by Wesley Smith on 9/25/12.
+//  Copyright (c) 2012 Wesley Smith. All rights reserved.
+//
+
+#import "LUPhotoViewController.h"
+
+@interface LUPhotoViewController ()
+
+@end
+
+@implementation LUPhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
